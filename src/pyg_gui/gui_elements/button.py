caa7e544e0441a06143a03e78a77ec29b71fc059
Almale02 <gaspardaniel080716@gmail.com>
2022-04-06 18:39:26 +0200

    v 0.9.1 (multiple event triggering is patched)

--- src/pyg_gui/gui_elements/button.py
+++ src/pyg_gui/gui_elements/button.py
@@ -5,13 +5,13 @@ import pygame
 
 
 class Button:
-    def __init__(self,window,pos,size,colour,txt = None, listeners = None, listener_atributes = ()):
+    def __init__(self,window,pos,size,colour,txt_obj = None, listeners = [], listener_atributes = []):
         self.window = window
         self.pos = pos
         self.size = size
         self.listeners = listeners
         self.colour = colour
-        self.txt = txt
+        self.txt_obj = txt_obj
         self.listener_atributes = listener_atributes
         ###
         self.surface = pygame.Surface([self.size[0],size[1]])
@@ -22,19 +22,22 @@ class Button:
     def draw(self):
         self.window.blit(self.surface,self.pos)
 
-    def check_press(self):
-        if pygame.mouse.get_pressed()[0]:
-            if self.rect.collidepoint(pygame.mouse.get_pos()):
-                for i_funcs,funcs in enumerate(self.listeners):
-                    funcs(self.listener_atributes[i_funcs])
+    def check_press(self,e):
+        if pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.rect.collidepoint(pygame.mouse.get_pos()):
+                    for i_funcs,funcs in enumerate(self.listeners):
+                        funcs(self.listener_atributes[i_funcs])
 
 
-    def update(self):
+    def non_event_update(self):
         self.surface = pygame.Surface([self.size[0], self.size[1]])
         self.rect = pygame.Rect(self.pos,self.size)
         self.surface.fill(self.colour)
         self.draw()
-        self.check_press()
+
+    def event_update(self,e):
+        self.check_press(e)
 
 
 
--- tests/button_test/test.py
+++ tests/button_test/test.py
@@ -3,6 +3,7 @@ import time
 import pygame,sys
 from src.pyg_gui.gui_elements.button import Button
 
+
 def test(atr):
     print(atr[0])
 
@@ -16,17 +17,26 @@ window = pygame.display.set_mode((1000,800))
 button = Button(window,(10,300),(400,36),(130,44,111),listeners=[test,k],listener_atributes=[("1"),("2")])
 
 buttons = [button]
-def update_buttons(buttons):
+def update_non_event_fcs(buttons):
     for i in buttons:
-        i.update()
+        i.non_event_update()
+
+
+def update_event_fcs(buttons,e):
+    for i in buttons:
+        i.event_update(e)
+
+
+
 
 while True:
     for e in pygame.event.get():
+        update_event_fcs(buttons,e)
         if e.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    update_buttons(buttons)
+    update_non_event_fcs(buttons)
 
     pygame.display.flip()
     pygame.display.update()
